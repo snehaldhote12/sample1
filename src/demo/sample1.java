bf06a4bb3dc90eb2c912c7502b3966f3fdf0b5ef
user <user@dell>
2022-10-29 19:53:28 +0530

    commit github

---
+++ src/demo/sample1.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class sample1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Good morning");
+		m1();
+	}
+	
+	public static void m1()
+	{
+		System.out.println("m1 is static method");
+	}
+}
